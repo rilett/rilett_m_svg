ebf4f9c9f4a92813fede73a6b1783c90966ff0c9
Rilett <m_rilett@tss-hr420-dm37.local>
2019-01-21 16:01:31 -0500

    added files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(() => {
+	// self invoking anonymous function (or Lambda)
+	// var => means create a variable
+	// whatever comes after the = is what the variable reads
+	var images = document.querySelectorAll('img');
+	//document is always the webpage
+	// after the dot is alway a method or a property
+
+	console.log(images);
+
+})();
